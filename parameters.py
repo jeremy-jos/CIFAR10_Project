3f46b5480144d850f083d45903ba1d712b1a5864
Jeremy Joslove <jeremy.joslove@gmail.com>
2019-11-26 15:07:56 +0100

    Load cifar10 data from keras

---
+++ parameters.py
@@ -0,0 +1,5 @@
+TRAINING_PARAMETERS = {
+    'num_classes': 10,
+    'batch_size': 32,
+    'num_epochs': 100,
+}
--- src/load_data.py
+++ src/load_data.py
@@ -1,6 +1,16 @@
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+from keras.datasets import cifar10
+
+
+def load_data():
+    """
+    Function to load the cifar10 dataset. Rather than downloading the files from the project's website, I use the
+        dataset already available in Keras out of simplicity.
+
+    Returns:
+       Tuple containing the training data and labels, and the testing data and labels
+    """
+
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    return (x_train, y_train), (x_test, y_test)
 
