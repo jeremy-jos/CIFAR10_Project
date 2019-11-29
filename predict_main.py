3fffaa9a9444d4383225ac9b57c0a4ab898f0517
Jeremy Joslove <jeremy.joslove@gmail.com>
2019-11-29 11:57:38 +0100

    Corrected bug in prediction

--- predict_main.py
+++ predict_main.py
@@ -4,15 +4,13 @@ from src.predict import load_saved_model, predict_class, load_image
 # Main file to run to perform a prediction using a model and an image defined below
 
 
-model_name = "test_model_new"
-image_name = "car"
+model_name = "2_blocks_20_epochs"
+image_name = "automobile"
 
 # Load model and image
 model = load_saved_model(model_name)
 image = load_image(image_name)
 
 # Obtain network predictions
-predictions = predict_class(image, model)
-
-# Process network prediction
-print(predictions[0])
+prediction = predict_class(image, model)
+print(prediction)
